e9359d29b36b00f0d479f5072de6a7780f521fd3
Artur <ar-2@tut.by>
2017-11-15 17:46:11 +0300

    update

--- SoftwereEnginiring/task_5/MLFQ.py
+++ SoftwereEnginiring/task_5/MLFQ.py
@@ -4,9 +4,9 @@ Implementation of the Multilevel Feedback Queue scheduler
 from simso.core import Scheduler, Timer
 from simso.schedulers import scheduler
 
+QUANTUM = 8
 BOOST_DELAY = 200
 NUM_QUEUE = 4
-
 
 @scheduler(
     "simso.schedulers.MLFQ",
@@ -16,12 +16,12 @@ NUM_QUEUE = 4
         'default': '0'
     }])
 class MLFQ(Scheduler):
-    """Round Robin scheduler"""
+    """Multilevel Feedback Queue scheduler"""
 
     def init(self):
 
         self.ready_lists = [[] for _ in range(NUM_QUEUE)]
-        self.timer = None
+        self.job_timer = None
         self.boost_timer = Timer(
             self.sim,
             MLFQ.boost_event, (self, self.processors[0]),
@@ -30,14 +30,6 @@ class MLFQ(Scheduler):
             cpu=self.processors[0],
             in_ms=True)
         self.interupt = False
-
-    def boost_event(self, cpu):
-        for r in range(len(self.ready_lists) - 1, 0, -1):
-            self.ready_lists[r - 1].extend(self.ready_lists[r])
-        
-        for j in self.ready_lists[0]:
-            j.data["priority"] = 0
-        cpu.resched()
 
     def on_activate(self, job):
         self.ready_lists[0].append(job)
@@ -51,39 +43,55 @@ class MLFQ(Scheduler):
             self.timer.stop()
         job.cpu.resched()
 
+    def boost_event(self, cpu):
+        for q in range(1, NUM_QUEUE):
+            for j in self.ready_lists[q]:
+                j.data["priority"] = 0
+            self.ready_lists[0].extend(self.ready_lists[q])
+        cpu.running.data["priority"] = 0
+        cpu.resched()
+
     def end_event(self, cpu):
         self.interupt = True
         self.timer.stop()
+        prior = cpu.running.data["priority"]
+        cpu.running.data["priority"] = prior + 1 if prior < NUM_QUEUE - 1 else NUM_QUEUE - 1
         cpu.resched()
 
-    def schedule(self, cpu):
-        if self.ready_lists or self.interupt: 
-            if self.interupt:
-                self.interupt = False
-                prior = cpu.running.data["priority"]
-                cpu.running.data["priority"] = prior + 1 if prior < len(
-                    self.ready_lists) else NUM_QUEUE - 1 
-                self.ready_list[cpu.running.data["priority"]].append(
-                                cpu.running)
-                self.timer = self.getTimer(cpu.running.data["priority"])
-                self.timer.start()
+    def getTimer(self, prior):
+        if prior < NUM_QUEUE-1:
+            return Timer(
+                self.sim,
+                MLFQ.boost_event, (self, self.processors[0]),
+                QUANTUM*(2**prior),
+                cpu=self.processors[0],
+                in_ms=True)
+        else:
+            None
 
+    def schedule(self, cpu):
+        if self.interupt or any(self.ready_lists):
             job = None
-            for r in range(len(self.ready_lists)):
-                if self.ready_lists[r]:
-                    job = self.ready_lists[r][0]
-                    break
+            if any(self.ready_lists):
+                for r in range(NUM_QUEUE):
+                    if self.ready_lists[r]:
+                        job = self.ready_lists[r][0]
+                        break
+            else:
+                job = cpu.running
+
+            prior = job.data["priority"]
 
             if (not cpu.running or
-                    job.data["priority"] < cpu.running.data["priority"]):
-
-                self.timer = self.getTimer(job.data["priority"])
+                    prior < cpu.running.data["priority"] or
+                    self.interupt):
+                self.timer = self.getTimer(prior)
                 self.timer.start()
-                self.ready_lists[r].remove(job)
+                if job in self.ready_lists[prior]:
+                    self.ready_lists[prior].remove(job)
                 if cpu.running:
-                    self.ready_list[cpu.running.data["priority"]].append(
+                    self.ready_lists[cpu.running.data["priority"]].append(
                         cpu.running)
                 return (job, cpu)
-
         else:
             return None
