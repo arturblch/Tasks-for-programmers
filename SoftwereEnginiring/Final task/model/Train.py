8f4bf7c3b0adbec3592e5b41277f2f75bc2b6f7f
Arthur <ar-2@tut.by>
2017-11-25 22:23:35 +0200

    Change class world

---
+++ SoftwereEnginiring/Final task/model/Train.py
@@ -0,0 +1,9 @@
+class Train:
+    def __init__(self, response):
+        self.capacity = response['capacity']
+        self.idx = response['idx']
+        self.line_idx = response['line_idx']
+        self.player_id = response['player_id']
+        self.position = response['position']
+        self.product = response['product']
+        self.speed = response['speed']
